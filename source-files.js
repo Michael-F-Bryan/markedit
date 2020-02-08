ab1d26188a64e148dfaa13a9f57d2f4338767185
Michael Bryan (via Travis CI) <michaelfbryan@gmail.com>
2020-02-08 10:20:37 +0000

    Deploy Michael-F-Bryan/markedit to github.com/Michael-F-Bryan/markedit.git:gh-pages

--- source-files.js
+++ source-files.js
@@ -1,7 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["bitflags"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["getopts"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["markedit"] = {"name":"","dirs":[{"name":"matchers","files":["heading.rs","mod.rs","one_shot.rs","start_of_next_line.rs"]},{"name":"rewriters","files":["mod.rs","rewritten.rs"]}],"files":["lib.rs"]};
+sourcesIndex["markedit"] = {"name":"","dirs":[{"name":"matchers","files":["and.rs","falling_edge.rs","heading.rs","mod.rs","one_shot.rs","start_of_next_line.rs"]},{"name":"rewriters","files":["mod.rs","rewritten.rs","writer.rs"]}],"files":["lib.rs"]};
 sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","lib.rs","naive.rs"]};
 sourcesIndex["pulldown_cmark"] = {"name":"","files":["entities.rs","escape.rs","html.rs","lib.rs","linklabel.rs","parse.rs","puncttable.rs","scanners.rs","strings.rs","tree.rs"]};
 sourcesIndex["unicase"] = {"name":"","dirs":[{"name":"unicode","files":["map.rs","mod.rs"]}],"files":["ascii.rs","lib.rs"]};
